1d882ea4efcbe2b916c56c0ce4d4c05fbce449be
webild <admin@webild.io>
2025-08-25 11:04:43 +0300

    Update project files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { ReactNode } from 'react';
 import '@/app/globals.css';
-import { interTight } from '@/utils/fonts';
-import { manrope } from '@/utils/fonts';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${interTight.variable} ${manrope.variable} antialiased bg-gradient-to-br from-[#EAF6FF] to-[#FFFFFF]`}>{children}</body>
+      <body className="antialiased bg-gradient-to-br from-[#EAF6FF] to-[#FFFFFF]">{children}</body>
     </html>
   );
 }
